eaf4a16adf4216d23b9e5ae45579d5ed599afe33
Kevin Cunnane <kevcunnane@gmail.com>
2018-08-10 12:35:39 -0700

    Hello Connected World implementation

--- src/extension.ts
+++ src/extension.ts
@@ -12,19 +12,22 @@ import * as sqlops from 'sqlops';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloconnectedworld" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
+    let disposable = vscode.commands.registerCommand('extension.sayHello', async () => {
+        // use async call to get the connection info
+        let connection = await sqlops.connection.getCurrentConnection();
+        if (!connection) {
+            let allConnections = await sqlops.connection.getActiveConnections();
+            if (allConnections && allConnections.length > 0) {
+                connection = allConnections[0];
+            }
+        }
+        // Format the message
+        let message = `Hello Connected World! Please connect in Object Explorer or a Query window`;
+        if (connection) {
+            message = `Hello Connected World! Your server name is ${connection.options['server']}`;
+        }
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage(message);
     });
 
     context.subscriptions.push(disposable);
